06001d39c02000e92f59bbb2407f5decda29e313
Etio <etiotan@gmail.com>
2017-04-28 12:26:51 -0400

    prettier

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
-
-
 import Web3 from 'web3'
 import  * as _  from './contracts/adder.js'
+import Notifications, {notify} from 'react-notify-toast';
 
 
-
+//connect to test-net
 var web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 
 //returns current address
@@ -19,7 +18,7 @@ var MyContract = web3.eth.contract(_.abi);
 // instantiate by address
 var contractInstance = MyContract.at(_.contractAddress);
 
-        //# of transactions made by account
+//# of transactions made by account
 var transactionCount = web3.eth.getTransactionCount(coinbase)
 
 //display doesnt count as a transaction as long as you call it
@@ -34,8 +33,6 @@ class App extends Component {
 
     componentWillMount(){
         console.log(web3, 'loaded')
-
-
     }
 
 
@@ -44,26 +41,34 @@ class App extends Component {
 
            console.log(`current count is ${display[1].toNumber()+1}, it'll update give it time`)
           contractInstance.adder({from:coinbase})
+          notify.show('Added! :)', "success", 3000);
+
       }
       function _subtractor(){
           console.log(`current count is ${display[1].toNumber()-1}`)
           contractInstance.subtractor({from:coinbase})
-
+          notify.show('Subtracted! :(', "error", 3000);
       }
 
 
     return (
-      <div className="App">
-          <h1>Your Address: {coinbase}</h1>
-          <h2>Monies: {balance} Ethers</h2>
-          <h2>Total Transactions: {transactionCount}</h2>
-          <h1>Current Count is at: {display[1].toNumber()}</h1>
-          <h1><a href='https://ropsten.etherscan.io/address/0xec2f06fcef81c913ee857a62cea6a592e58db241'>View Contract</a></h1>
-          <button onClick={_adder}>Adder</button>
+      <div className="container">
 
-          <button onClick={_subtractor}>Subtractor</button>
+          <h3>Decentralized Counter</h3>
+          <ul>
+          <li>Your Address: {coinbase}</li>
+          <li>Monies: {balance} Ethers</li>
+      <blockquote className="count">
+          <li>Total Transactions: {transactionCount}</li>
+          <li>Current Count is at: {display[1].toNumber()}</li>
+      </blockquote>
+          </ul>
 
+          <button className="waves-effect waves-light btn" onClick={_adder}>ADD</button>
 
+          <button className="waves-effect red btn" onClick={_subtractor}>SUB</button>
+
+          <Notifications />
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import './styles.css';
+import {Card} from './Components/Card'
 
+
+class MainLayout extends React.Component{
+    render(){
+        return(
+    <div>
+        <App />
+        <Card />
+    
+    </div>
+        )
+    }
+}
 
 
 
 ReactDOM.render(
-  <App />,
+  <MainLayout />,
   document.getElementById('root')
 );
