ebc2e9608183a759ebc54d2cc9df5996ebea28a6
Etio <etiotan@gmail.com>
2017-04-30 00:32:02 -0400

    Sending works!

---
+++ src/contracts/send.js
@@ -0,0 +1,3 @@
+import { web3, coinbase } from './connect.js'
+
+web3.eth.sendTransaction({from: })
--- src/pages/Send.js
+++ src/pages/Send.js
@@ -1,7 +1,21 @@
 import React from 'react';
 import {Information} from '../contracts/connect.js';
+import {web3, coinbase } from '../contracts/connect.js'
 
-export const Send = () => {
+export class Send extends React.Component {
+
+handleSend(event){
+    event.preventDefault();
+    var address = (this.refs.address.value)
+    var amount = (this.refs.amount.value)
+
+    this.refs.address.value= ""
+    this.refs.amount.value= ""
+    console.log(web3.eth.sendTransaction({from: coinbase, to: address, value: web3.toWei(amount, 'ether'), gasLimit:21000, gasPrice:2000000000}))
+    alert('Transaction Sent, View Console & wait for transaction to be mined')
+}
+
+    render(){
     return (
         <div className="container col s9 right flow-text">
 
@@ -9,19 +23,24 @@ export const Send = () => {
             <Information/>
 
             <div className="input-field col s7">
-                <input type="text" className="validate"/>
+                <input ref="address" type="text" className="validate"/>
                 <label>Send To:</label>
             </div>
 
             <div className="input-field col s7">
-                <input type="text" className="validate"/>
+                <input ref="amount" type="text" className="validate"/>
                 <label>Amount</label>
             </div>
             <div className="col s7">
-                <button className="btn waves-effect waves-light" type="submit" name="action">Send
+                <button onClick={this.handleSend.bind(this)} className="btn waves-effect waves-light" type="submit">Send
                     <i className="material-icons right">send</i>
                 </button>
             </div>
+            <div className='col s7'>
+            <p className="red-text">Costs {web3.fromWei("42000000000000", 'ether')} Ether to send</p>
+            <p>View your transaction getting mined <a href={`https://ropsten.etherscan.io/address/${coinbase}`} target='_blank'>here</a></p>
+        </div>
         </div>
     )
+}
 }
